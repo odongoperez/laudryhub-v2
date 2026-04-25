9be1020831850b39c3c89352048f93aeb9381a90
odongoperez <odongoperez@users.noreply.github.com>
2026-04-25 18:48:05 +0200

    Show 'Machine in use' banner when wash is unattributable + by-user label on widget

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v7-stable-modals";
+const VERSION = "v8-machine-started-attribution";
 const STATIC_CACHE = `laundryhub-static-${VERSION}`;
 const RUNTIME_CACHE = `laundryhub-runtime-${VERSION}`;
 
