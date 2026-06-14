ebf87cfce6870b1dced6df67968bff6140ef1e2d
odongoperez <odongoperez@users.noreply.github.com>
2026-06-14 02:58:57 +0200

    Real theme system: 4 styles, 7 fonts, 3 densities, 3 corner shapes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v15-settings-cleanup";
+const VERSION = "v16-real-themes-fonts-density";
 const STATIC_CACHE = `laundryhub-static-${VERSION}`;
 const RUNTIME_CACHE = `laundryhub-runtime-${VERSION}`;
 
